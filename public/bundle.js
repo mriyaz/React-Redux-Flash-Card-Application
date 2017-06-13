63d5631dfb14a22f0f416752bb0eb124b72efbec
Riyazuddin M <riyaz4ug@gmail.com>
2017-06-13 11:36:23 +1000

    Updated files

--- public/bundle.js
+++
@@ -1,1 +0,0 @@
-console.error("SyntaxError: E:/MyProjects/ReactProjects/flashcards-app/src/components/VisibleCards.js: const is a reserved word (8:3) while parsing file: E:\\MyProjects\\ReactProjects\\flashcards-app\\src\\components\\VisibleCards.js");
